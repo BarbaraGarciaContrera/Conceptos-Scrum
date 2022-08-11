27e7990aeec3eef9c8a4e8c07f89f4e4de2dc581
Julián Meneses <104313679+JulianMeneses1@users.noreply.github.com>
2022-08-11 20:21:05 -0300

    Update and rename Ejercicio 2 SQL.sql to Script.sql

--- Script.sql
+++ Script.sql
@@ -1,5 +1,8 @@
 create database Peluquería_Canina;
 use Peluquería_Canina;
+
+# Tabla Dueño
+
 create table Dueno (
 DNI int primary key,
 Nombre varchar(30),
@@ -8,6 +11,9 @@ Telefono varchar(50),
 Direccion varchar(45)
 );
 insert into Dueno values (40518042,"Julián","Meneses","3516565702","Fitz Roy 2000");
+
+# Tabla Perro
+
 create table Perro(
 ID_Perro int primary key,
 Nombre varchar(30),
@@ -17,6 +23,10 @@ DNI_dueno int,
 foreign key (DNI_dueno) references Dueno (DNI)
 );
 insert into Perro values (01,"Bartok","2010-12-20","Macho",40518042);
+update Perro set Fecha_nac="2009-11-09" where ID_Perro=01;
+
+# Tabla Historial
+
 create table Historial(
 ID_Historial int primary key,
 Fecha date,
