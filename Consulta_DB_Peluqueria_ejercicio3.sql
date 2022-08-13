1acfeb94923e408987621ebd083f813fe5853b7f
C-Adrian-A <87579549+C-Adrian-A@users.noreply.github.com>
2022-08-12 23:09:09 -0300

    Add files via upload
    Se agrega consulta SQL a db_Peluquería Valoración final

---
+++ Consulta_DB_Peluqueria_ejercicio3.sql
@@ -0,0 +1,7 @@
+# Se realiza una consulta al historial de db_Peluquería para detectar pacientes que no se atienden hace tiempo
+
+select * from Historial;
+
+# Se elimina un paciente 
+
+delete from Historial where Fecha = '21-05-20';
