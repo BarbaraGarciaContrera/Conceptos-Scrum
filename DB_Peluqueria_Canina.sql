6450372ef2f0dccc7ba6cc00938260820c8633f6
C-Adrian-A <87579549+C-Adrian-A@users.noreply.github.com>
2022-08-12 23:07:09 -0300

    Add files via upload
    Se agrega Base De Datos Peluquería Valoración Final

---
+++ DB_Peluqueria_Canina.sql
@@ -0,0 +1,45 @@
+# Se crea la base de datos db_Peluquería
+
+create database if not exists db_Peluquería;
+
+# Se pone en uso la DB
+
+use db_Peluquería;
+
+# Se crea la tabla Dueno
+
+create table if not exists Dueno (
+DNI int primary key not null,
+Nombre varchar(30),
+Apellido varchar(30),
+Telefono varchar(50),
+Direccion varchar(45)
+);
+
+# Se crea la tabla Perro
+
+create table if not exists Perro(
+ID_Perro int primary key,
+Nombre varchar(30),
+Fecha_nac date,
+Sexo varchar(10),
+DNI_dueno int,
+index fk_Dueno_idx (DNI_dueno ASC) visible,
+CONSTRAINT fk_DNI_dueno foreign key (DNI_dueno) references Dueno (DNI));
+
+# Se crea la tabla Historial
+
+create table Historial(
+ID_Historial int primary key not null,
+Fecha date,
+Descripcion varchar(100),
+Monto int not null,
+Perro int not null,
+index fk_Perro_idx (Perro ASC) VISIBLE,
+constraint fk_Perro foreign key (Perro) references Perro (ID_Perro)
+);
+
+
+
+
+
